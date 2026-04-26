2a7cbdd318de202567ac2eeada93e725c9ac15d3
Gusky76 <petricagascan76@gmail.com>
2026-04-26 20:23:05 +0300

    Implementat functie simpla LED

--- src/servocontrol.c
+++ src/servocontrol.c
@@ -7,6 +7,9 @@
 #define SERVO_PORT GPIO_PORTB
 #define SERVO_PIN  1
 
+#define LED_PORT   GPIO_PORTB
+#define LED_PIN    3   
+
 #define POT_CHANNEL 0
 
 void ServoControl_Init(void)
@@ -15,16 +18,24 @@ void ServoControl_Init(void)
 
     GPIO_Init(SERVO_PORT, SERVO_PIN, GPIO_OUTPUT);
     PWM_Init(SERVO_PORT, SERVO_PIN, 50);
+
+    GPIO_Init(LED_PORT, LED_PIN, GPIO_OUTPUT);
+    PWM_Init(LED_PORT, LED_PIN, 500); 
 }
 
 void ServoControl_Update(void)
 {
     uint16_t adc_value;
-    uint8_t duty;
+    uint8_t servo_duty;
+    uint8_t led_duty;
+    uint16_t distance;
 
     adc_value = ADC_Read(POT_CHANNEL);
 
-    duty = 13 + ((adc_value * (26 - 13)) / 1023);
+    servo_duty = 13 + ((adc_value * (26 - 13)) / 1023);
+    PWM_SetDutyCycle(SERVO_PORT, SERVO_PIN, servo_duty);
 
-    PWM_SetDutyCycle(SERVO_PORT, SERVO_PIN, duty);
+    distance = (adc_value > 512) ? (adc_value - 512) : (512 - adc_value);
+    led_duty = (distance * 255) / 512;
+    PWM_SetDutyCycle(LED_PORT, LED_PIN, led_duty);
 }
